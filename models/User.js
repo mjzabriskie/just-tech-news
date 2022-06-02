1bdf155ce4da4aeb169e7951e9a7b719068997ca
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-06-02 01:31:14 -0600

    added ability to hash passwords before they are saved into the database

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -36,6 +37,18 @@ User.init(
         }
     },
     {
+        hooks: {
+            //set up before create lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            //set up before update lifecycle hook functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
