5d17e510b6d5acf7e34dad4179ac1d8a8b1d7f69
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-06-19 01:12:18 -0600

    Fixed handlebars reference to css stylesheet

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const path = require("path");
-const express = require("express");
-const exphbs = require("express-handlebars");
-const routes = require("./controllers");
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
+const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
 
@@ -15,10 +14,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log('Now listening'));
 });
