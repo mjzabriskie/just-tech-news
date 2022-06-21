96f608e2dfd3c29a5d29ace50a49f643f99d2dd0
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-06-20 23:35:32 -0600

    added dashboard along with routes and logic to support the functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
+const withAuth = require('../../utils/auth');
 
 //Get all posts
 router.get("/", (req, res) => {
@@ -88,11 +89,11 @@ router.get("/:id", (req, res) => {
 });
 
 //POST new post
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id,
+    user_id: req.session.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -102,7 +103,7 @@ router.post("/", (req, res) => {
 });
 
 //PUT /api/posts/upvote
-router.put("/upvote", (req, res) => {
+router.put("/upvote", withAuth, (req, res) => {
   //make sure the session exists first
   if (req.session) {
     //pass session id along with all destructured properties on req.body
@@ -119,7 +120,7 @@ router.put("/upvote", (req, res) => {
 });
 
 //PUT update post title
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -144,7 +145,7 @@ router.put("/:id", (req, res) => {
 });
 
 //DELETE a post
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
